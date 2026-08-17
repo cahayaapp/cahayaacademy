b542e52caa3c9357ac491ffc44d085a5b3e70c90
cahayaapp <cahayafajrulislam@gmail.com>
2026-08-17 23:11:36 +0700

    new

--- assets/js/pwa-root.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    try {
-      const registration = await navigator.serviceWorker.register('./sw.js?v=646');
-      await registration.update();
-    } catch (_) {}
-  });
-}
